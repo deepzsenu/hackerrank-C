a8260525394ffe5dfae342ab75c78462f3b42f75
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-02-19 16:26:36 +0530

    Create sum_and_difference_of_two_number.c

---
+++ Basic/sum_and_difference_of_two_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,e,f;
+    float c,d,g,h;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%f",&c);
+    scanf("%f",&d);
+    e = a+b;
+    f = a-b;
+    g = c+d;
+    h = c-d;
+    printf("%d %d",e,f);
+    printf("\n%0.1f %0.1f",g,h);
+    
+
+	
+    
+    return 0;
+}
